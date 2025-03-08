d4e55352f290e9b8b6ac1f0dbc58a533edbb1280
Donya Mirabdolahi <donya.mirabdolahi@gmail.com>
2025-03-08 17:58:23 +0330

    Lina Story Component - Desktop/Mobile

---
+++ src/views/About/LinaStory/linaStory.tsx
@@ -0,0 +1,25 @@
+"use client";
+import { useEffect } from "react";
+import { LinaStoryMobile } from "./linaStoryMobile";
+import { LinaStoryDesktop } from "./linaStoryDesktop";
+import { MOBILE_BREAKPOINT, useDeviceStore } from "@/shared/model";
+
+
+const LinaStory = () => {
+  const { isMobile, setIsMobile } = useDeviceStore();
+
+  useEffect(() => {
+    const checkDevice = () => {
+      const width = window.innerWidth;
+      setIsMobile(width <= MOBILE_BREAKPOINT);
+    };
+
+    checkDevice();
+    window.addEventListener("resize", checkDevice); 
+    return () => window.removeEventListener("resize", checkDevice);
+  }, [setIsMobile]);
+
+  return isMobile ? <LinaStoryMobile /> : <LinaStoryDesktop />;
+};
+
+export default LinaStory;
